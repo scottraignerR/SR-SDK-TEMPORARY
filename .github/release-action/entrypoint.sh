e96d79708d4b22f87771d210074c856b87d509f7
Mike Hamilton <55596494+MikeHamilton-RW@users.noreply.github.com>
2019-11-23 16:51:47 -0800

    Update entrypoint.sh

--- .github/release-action/entrypoint.sh
+++ .github/release-action/entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 hub checkout master
-#VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle`
 MAJOR_VERSION=`cat build.gradle | grep 'majorVersion = ' | awk '{print $3}'`
 MINOR_VERSION=`cat build.gradle | grep 'minorVersion = ' | awk '{print $3}'`
 PATCH_VERSION=`cat build.gradle | grep 'patchVersion = ' | awk '{print $3}'`
 VERSION_NAME=${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}.${BUILD_NUMBER}
-# hub release create -a ./${APP_FOLDER}/build/outputs/${OUTPUT_TYPE}/*.${OUTPUT_TYPE} -m "${RELEASE_TITLE}_v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
 hub release create -a ./${APP_FOLDER}/build/outputs/${OUTPUT_TYPE}/*.${OUTPUT_TYPE} -m "${RELEASE_TITLE}_v${VERSION_NAME}" v${VERSION_NAME}
