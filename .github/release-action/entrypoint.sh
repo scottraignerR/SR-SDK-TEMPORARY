45bd926808b037725e1a392d6af32ebf07bca954
Mike Hamilton <55596494+MikeHamilton-RW@users.noreply.github.com>
2019-11-25 06:51:36 -0800

    Update entrypoint.sh

--- .github/release-action/entrypoint.sh
+++ .github/release-action/entrypoint.sh
@@ -5,7 +5,6 @@ MAJOR_VERSION=`cat build.gradle | grep 'majorVersion = ' | awk '{print $3}'`
 MINOR_VERSION=`cat build.gradle | grep 'minorVersion = ' | awk '{print $3}'`
 PATCH_VERSION=`cat build.gradle | grep 'patchVersion = ' | awk '{print $3}'`
 VERSION_NAME=${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}.${BUILD_NUMBER}
-hub release create -a ./${APP_FOLDER}/build/outputs/${OUTPUT_TYPE}/*.${OUTPUT_TYPE} -m "${RELEASE_TITLE}_v${VERSION_NAME}" v${VERSION_NAME}
-
-# hub release create -a ./${APP_FOLDER}/build/outputs/${OUTPUT_TYPE}/*.${OUTPUT_TYPE} -m "${RELEASE_TITLE}_v${VERSION_NAME}" -t v${COMMIT_SHA} v${VERSION_NAME}
+# hub release create -a ./${APP_FOLDER}/build/outputs/${OUTPUT_TYPE}/*.${OUTPUT_TYPE} -m "${RELEASE_TITLE}_v${VERSION_NAME}" v${VERSION_NAME}
+hub release create -a ./${APP_FOLDER}/build/outputs/${OUTPUT_TYPE}/*.${OUTPUT_TYPE} -m "${RELEASE_TITLE}_v${VERSION_NAME}" -t v${COMMIT_SHA} v${VERSION_NAME}
 
