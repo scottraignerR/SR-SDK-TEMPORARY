191ae20635701e076d6d2efcffadab3eae5f419e
Mike Hamilton <55596494+MikeHamilton-RW@users.noreply.github.com>
2019-11-27 21:50:04 -0800

    Update entrypoint.sh

--- .github/release-action/entrypoint.sh
+++ .github/release-action/entrypoint.sh
@@ -9,6 +9,7 @@ VERSION_NAME=${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}.${BUILD_NUMBER}
 
 # FIND LAST RELEASE, GET HASH
 RESULT=$(curl -H "Authorization: token ${GITHUB_TOKEN}" "https://api.github.com/repos/realwear/WN-SDK-TEMPORARY/releases/latest")
+LAST_COMMMIT=$(echo $RESULT | grep -Po '"target_commitish":.*?[^\\]",')
 echo $RESULT | grep -Po '"target_commitish":.*?[^\\]",'
 
 # GET ALL HASHES UP TO CURRENT
@@ -28,6 +29,7 @@ echo "* Update branch_build_test_lint.yaml (#82) @MikeHamilton-RW" >> $FILENAME
 echo "* Update release-drafter.yaml (#81) @MikeHamilton-RW" >> $FILENAME
 echo "* merge to master (#80) @MikeHamilton-RW" >> $FILENAME
 echo "* Optimize action workflow (#79) @MikeHamilton-RW" >> $FILENAME
+echo "* Commit of last release: $LAST_COMMMIT"
 # Need to capture all previous commit meessages.
 hub release create -a ./${APP_FOLDER}/build/outputs/${OUTPUT_TYPE}/*.${OUTPUT_TYPE} -F $FILENAME -t ${COMMIT_SHA} v${VERSION_NAME}
 
